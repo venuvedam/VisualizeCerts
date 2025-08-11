4f813fc8c811c866f08756f34c9f1b181e918047
Guy Gregory <register@guygregory.com>
2025-08-11 17:35:16 +0100

    Update ai_exam_recommender.py
    Switched the GitHub Models endpoint to the new URL: 
    https://models.github.ai/inference
    
    Commented-out the old URL:
    https://models.inference.ai.azure.com

--- ai_exam_recommender.py
+++ ai_exam_recommender.py
@@ -9,8 +9,8 @@ import json
 # To authenticate with the model you will need to generate a personal access token (PAT) in your GitHub settings. 
 # Create your PAT token by following instructions here: https://docs.github.com/en/authentication/keeping-your-account-and-data-secure/managing-your-personal-access-tokens
 client = OpenAI(
-    #base_url="https://models.github.ai/inference",
-    base_url="https://models.inference.ai.azure.com",
+    base_url="https://models.github.ai/inference",
+    #base_url="https://models.inference.ai.azure.com",
 
     api_key=os.environ["GITHUB_TOKEN"],
 )
